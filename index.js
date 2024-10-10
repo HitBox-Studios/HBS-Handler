8667bf9ea2e4160b4d10943d8ab9288975da1a8a
prosilentgamer2 <fearlesstechofficial@gmail.com>
2024-10-10 00:53:50 -0500

    Added Argument Handling, and Added a Database.

--- index.js
+++
@@ -1,1 +0,0 @@
-console.log('Hello from command-handler/index.js');
---
+++ src/index.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const CommandHandler = require('./CommandHandler')
+
+class Main {
+  constructor({ client, mongoUri ,commandsDir }) {
+    if (!client) {
+      throw new Error('A client is required.  Error Code: 56351')
+    }
+
+    if (mongoUri) {
+        this.connectToMongo(mongoUri)
+      }
+
+    if (commandsDir) {
+      new CommandHandler(commandsDir, client)
+    }
+  }
+
+  connectToMongo(mongoUri) {
+    mongoose.connect(mongoUri, {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    // keepAlive: true,
+    })
+  }
+}
+
+module.exports = Main
